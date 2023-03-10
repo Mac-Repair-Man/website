48f7de3e005e2c1e871c564cac9cfe953792836c
Kay <davidanton037@gmail.com>
2023-03-09 22:05:36 -0500

    Added frontend

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 "use strict";
 
-console.log();
-console.log();
-console.log(" --- PROCESS INITIALIZATION ---");
-console.log("Time:", Date.now());
-
 const express = require("express"),
     ejs = require("ejs"),
     app = express(),
-    config = require("./config.json");
+    config = require("./config.json"),
+    moment = require("moment"),
+    chalk = require('chalk'),
+    log = console.log;
+
+log();
+log();
+log(chalk.green(" --- PROCESS INITIALIZATION ---"));
+log("Time:", Date.now());
+
+global.__base = __dirname + "/";
+log("__base:", __base);
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+app.use(express.static("static"));
 app.set("env", "development");
+
+app.use(logRequests);
+
 
 // Main home page
 app.get("/", function (req, res) {
@@ -31,14 +41,28 @@ function getTimeFormatted() {
     return moment().format("MMMM Do YYYY, h:mm:ss a") + " (" + Date.now() + ")";
 }
 
+function logRequests(req, res, next) {
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    log();
+    log(chalk.green("--- NEW REQUEST ---"));
+    log(chalk.yellow(
+        "Time:",
+        moment().format("MMMM Do YYYY, h:mm:ss a"),
+        "(" + Date.now() + ")"
+    ));
+    log(chalk.blue("IP: ", ip));
+    log(chalk.blue("Request:", req.originalUrl));
+    next();
+}
+
 const port = config.server.port || 5000;
 const server = app
     .listen(port, () => {
-        console.log();
-        console.log("--- WEBSERVER ON ---");
-        console.log("Listening at http://" + config.server.domain + ":" + port);
-        console.log();
+        log();
+        log(chalk.green("--- WEBSERVER ON ---"));
+        log(chalk.yellow("Listening at http://" + config.server.domain + ":" + port));
+        log();
     })
     .on("error", (err) => {
-        console.log("Connection error:", err);
+        log(chalk.red("Connection error:", err));
     });
