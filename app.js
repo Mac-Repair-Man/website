8372c7f69e481a64f74cc01f68279a504da99f7c
KayTwenty <davidanton037@gmail.com>
2023-04-03 16:42:44 -0400

    Refactored the backend

--- app.js
+++ app.js
@@ -1,94 +1,46 @@
 "use strict";
 
-const express = require("express"),
-    ejs = require("ejs"),
-    app = express(),
-    bodyParser = require('body-parser'),
-    config = require("./config.json"),
-    moment = require("moment"),
-    chalk = require('chalk'),
-    nodemailer = require('nodemailer'),
-    compression = require("compression"),
-    cookieParser = require('cookie-parser'),
-    morgan = require('morgan'),
-    log = console.log;
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require('body-parser');
+const config = require("./config.json");
+const router = require('./router');
+const contact = require('./contact');
+const moment = require("moment");
+const chalk = require('chalk');
+const compression = require("compression");
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+const log = console.log;
 
 log();
 log();
 log(chalk.green(" --- PROCESS INITIALIZATION ---"));
-log("Time:", Date.now());
+log("Time:", getTimeFormatted());
 
 global.__base = __dirname + "/";
 log("__base:", __base);
 
-
+// Middleware
 app.use(compression());
-app.set("view engine", "ejs");
-app.use(require("prerender-node"));
-app.set("views", __dirname + "/views");
+app.use(require('prerender-node'));
 app.use(express.static("static"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("env", "development");
 app.use(cookieParser());
 app.use(morgan('dev'));
 
-app.use(logRequests);
+// View engine setup
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-// Strict-Transport-Security header
-app.use(function(req, res, next) {
-    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload; SameSite=Strict');
-    next();
-});
+// Environment setup
+app.set("env", "development");
 
-// Home page
-app.get("/", function (req, res) {
-    res.render("pages/index");
-});
-
-// robots.txt redirect
-app.get("/robots.txt", function(req, res) {
-    res.sendFile(__dirname +  "/robots.txt");
-});
-
-// sitemap.xml redirect
-app.get("/sitemap.xml", function(req, res) {
-    res.sendFile(__dirname + "/sitemap.xml");
-});
-
-// 404 Page redirection
-app.use((req, res) => {
-    res.redirect("/");
-});
-
-// Contact middleware
-app.post('/contact', (req, res) => {
-    const transporter = nodemailer.createTransport({
-        host: config.contact.host,
-        port: config.contact.port || 465,
-        secure: config.contact.secure || true,
-        auth: {
-            user: config.contact.user,
-            pass: config.contact.pass
-        }
-    });
-
-    const mailOptions = {
-        from: 'info@macrepairman.ca',
-        to: 'applerepairmacman@gmail.com',
-        subject: 'New message from your website!',
-        text: 'You have received a new message from your website contact form.\n\nName: ' + req.body.name + '\nEmail: ' + req.body.email + '\nMessage: ' + req.body.message
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            res.send('An error occurred while sending the email.');
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.send('Thank you for your message!');
-        }
-    });
-});
+app.use('/', router);
+app.use('/', contact);
 
 // Server Functions
 function getTimeFormatted() {
@@ -97,37 +49,44 @@ function getTimeFormatted() {
 
 function logRequests(req, res, next) {
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    log();
-    log(chalk.green("--- NEW REQUEST ---"));
-    log(chalk.yellow(
-        "Time:",
-        moment().format("MMMM Do YYYY, h:mm:ss a"),
-        "(" + Date.now() + ")"
-    ));
-    log(chalk.blue("IP: ", ip));
-    log(chalk.blue("Request:", req.originalUrl));
+    console.log();
+    console.log();
+    console.log(chalk.green(" --- NEW REQUEST ---"));
+    console.log(chalk.yellow("Time:", getTimeFormatted()));
+    console.log(chalk.blue("IP: ", ip));
+    console.log(chalk.blue("Request:", req.originalUrl));
     next();
 }
 
-app.on('listening', function () {
-    console.log('Server started!');
+// Start server
+const port = config.server.port || 5000;
+const server = app.listen(port, () => {
+    log();
+    log(chalk.green("--- WEBSERVER ON ---"));
+    log(chalk.yellow("Listening at http://" + config.server.domain + ":" + port));
+    log();
+});
 
-    app.use(function (req, res, next) {
-        for (var cookie in req.cookies) {
-            res.clearCookie(cookie);
-        }
-        next();
+// Handle server errors
+server.on("error", (err) => {
+    log(chalk.red("Connection error:", err));
+});
+
+// Clear cookies on shutdown
+app.use(function (req, res, next) {
+    for (var cookie in req.cookies) {
+        res.clearCookie(cookie);
+    }
+    next();
+});
+
+// Handle server shutdown
+process.on("SIGINT", () => {
+    log(chalk.yellow("--- WEBSERVER OFF ---"));
+    server.close(() => {
+        process.exit(0);
     });
 });
 
-const port = config.server.port || 5000;
-const server = app
-    .listen(port, () => {
-        log();
-        log(chalk.green("--- WEBSERVER ON ---"));
-        log(chalk.yellow("Listening at http://" + config.server.domain + ":" + port));
-        log();
-    })
-    .on("error", (err) => {
-        log(chalk.red("Connection error:", err));
-    });
+// Add middleware
+app.use(logRequests);
---
+++ router.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+// Middleware to set the Strict-Transport-Security header
+router.use(function(req, res, next) {
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload; SameSite=Strict');
+    next();
+});
+
+// Routes
+router.get("/", function (req, res) {
+    res.render("pages/index");
+});
+
+router.get("/robots.txt", function(req, res) {
+    res.sendFile(__dirname + "/robots.txt");
+});
+
+router.get("/sitemap.xml", function(req, res) {
+    res.sendFile(__dirname + "/sitemap.xml");
+});
+
+// 404 handler
+router.use((req, res) => {
+    res.redirect("/");
+});
+
+module.exports = router;
