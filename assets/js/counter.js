559a5d264ebb279b280ec05171610307bb8fe0f0
Kay <davidanton037@gmail.com>
2023-05-30 10:46:42 -0400

    Better SEO Info

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,22 +1,33 @@
-// Get the current date and time
-const now = new Date();
-let count = 0;
-
-// Select the counter element
-const counterElement = document.getElementById('counter-daily');
-
-// Update the counter every hour
-setInterval(() => {
-    // Check if the current time is a new hour
-    if (new Date().getHours() !== now.getHours()) {
-        // Increment the counter
-        count++;
-
-        // Update the counter element with the new count
-        counterElement.textContent = count;
-
-        // Update the now variable to the current hour
-        now.setHours(new Date().getHours());
-    }
-}, 1000 * 60 * 60); // Run the code every hour (in milliseconds)
-
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
+  function updateCounter(counterElement) {
+    var currentValue = parseInt(counterElement.textContent);
+    var randomNumber = getRandomNumber(1, 10); // Adjust the range as needed
+    
+    var intervalId = setInterval(function() {
+      if (currentValue < currentValue + randomNumber) {
+        currentValue++;
+        counterElement.textContent = currentValue.toLocaleString();
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 100);
+  }
+  
+  window.addEventListener('DOMContentLoaded', function() {
+    var counterElements = document.querySelectorAll('.counter-number');
+    
+    counterElements.forEach(function(counterElement) {
+      counterElement.addEventListener('animationend', function() {
+        setTimeout(function() {
+          updateCounter(counterElement);
+          setInterval(function() {
+            updateCounter(counterElement);
+          }, 5000);
+        }, 100); // Adjust the delay as needed
+      });
+    });
+  });
+  
