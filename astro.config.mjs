690b315ecfeb866bec6029612b043affadb9ae9c
David Anton <davidanton037@gmail.com>
2024-04-04 09:08:38 -0400

    Performance optimizations
    - Added better loading for all images using Astro's new image components
    - Redefined the Navbar and Footer so it can be used universally
    - Added Astro-page-insight for debugging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import pageInsight from "astro-page-insight";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://macrepairman.ca',
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap(), pageInsight()]
 });
